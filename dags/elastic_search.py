3988f3803caefe9d5040f7d40e2d74916755b871
Szymon Głomski <szymon.glomski@gmail.com>
2024-04-21 18:18:34 +0200

    create basic language detection and translation

--- dags/elastic_search.py
+++ dags/elastic_search.py
@@ -13,7 +13,7 @@ default_args = {
 
 def fetch_data_from_elasticsearch():
     es_hook = ElasticsearchPythonHook(
-        hosts=["https://28e65033710b4d2e89093b2756a29c8b.us-central1.gcp.cloud.es.io"],
+        hosts=["https://172.18.0.2:9200/"],
         es_conn_args = {"api_key":  os.environ["ELASTIC_API_KEY"]}
         )
     query = {"query": {"term": {"ride_id": "65506F830C2B492D"}}}
@@ -21,7 +21,7 @@ def fetch_data_from_elasticsearch():
     print(result)
     return True
      
-with DAG('elasticsearch_example', default_args=default_args) as dag:
+with DAG('elasticsearch_example', default_args=default_args, schedule_interval=None) as dag:
     fetch_data_task = PythonOperator(
         task_id='fetch_data',
         python_callable=fetch_data_from_elasticsearch
