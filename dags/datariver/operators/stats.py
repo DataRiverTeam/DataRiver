5bb06891fe43cf11e2517f38fe8b6b58c9069f0e
Jakub <pilch@student.agh.edu.pl>
2024-06-13 00:47:08 +0200

    fix multiple files error

--- dags/datariver/operators/stats.py
+++ dags/datariver/operators/stats.py
@@ -12,25 +12,27 @@ class StatisticsOperator(BaseOperator):
         self.json_data = json_data
 
     def execute(self, context):
-        detected = ast.literal_eval(json.dumps(self.json_data[0]))
         label_counter = dict()
         entity_counter = dict()
 
-        for sentence in detected:
-            text = sentence["text"]
-            for ent in sentence["ents"]:
-                label = ent["label"]
-                if label in label_counter:
-                    label_counter[label] = label_counter[label] + 1
-                else:
-                    label_counter[label] = 1
-                start = ent["start"]
-                end = ent["end"]
-                entity = text[start:end]
-                if entity in entity_counter:
-                    entity_counter[entity] = entity_counter[entity] + 1
-                else:
-                    entity_counter[entity] = 1
+        for data in self.json_data:
+            detected = ast.literal_eval(json.dumps(data))
+
+            for sentence in detected:
+                text = sentence["text"]
+                for ent in sentence["ents"]:
+                    label = ent["label"]
+                    if label in label_counter:
+                        label_counter[label] = label_counter[label] + 1
+                    else:
+                        label_counter[label] = 1
+                    start = ent["start"]
+                    end = ent["end"]
+                    entity = text[start:end]
+                    if entity in entity_counter:
+                        entity_counter[entity] = entity_counter[entity] + 1
+                    else:
+                        entity_counter[entity] = 1
 
         returned_tuple = (label_counter, entity_counter)
         return returned_tuple
