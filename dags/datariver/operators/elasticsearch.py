88762e34f7e0712c2e342e4807606c3a3b0413e2
Szymon Głomski <102425825+DavidJones1102@users.noreply.github.com>
2024-06-13 18:57:39 +0200

    Elastic operator (#6)
    * WIP
    
    * elastic basic operators
    
    * cleanup
    
    * place all elastic operators in one file

--- dags/datariver/operators/elasticsearch.py
+++ dags/datariver/operators/elasticsearch.py
@@ -1,3 +1,54 @@
 from airflow.models.baseoperator import BaseOperator
 
-pass
+class ElasticPushOperator(BaseOperator):
+    template_fields = ("document",)
+
+    def __init__(self, *, index, document, fs_conn_id="fs_default",es_conn_args={}, **kwargs):
+        super().__init__(**kwargs)
+        self.fs_conn_id = fs_conn_id
+        self.index = index
+        self.document = document
+        self.es_conn_args = es_conn_args
+
+    def execute(self, context):
+        from elasticsearch import Elasticsearch
+
+        es = Elasticsearch(
+            **self.es_conn_args
+        )
+        es.index(
+            index=self.index,
+            document=self.document
+        )
+        es.indices.refresh(index=self.index)
+
+class ElasticSearchOperator(BaseOperator):
+
+    def __init__(
+            self,
+            *,
+            index,
+            query={ "match_all" : {} },
+            fs_conn_id="fs_default",
+            es_conn_args={},
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.fs_conn_id = fs_conn_id
+        self.index = index
+        self.query = query
+        self.es_conn_args = es_conn_args
+
+    def execute(self, context):
+        from elasticsearch import Elasticsearch
+
+        es = Elasticsearch(
+            **self.es_conn_args
+        )
+        result = es.search(
+            index=self.index,
+            query=self.query
+        )
+
+        return result.body
+
