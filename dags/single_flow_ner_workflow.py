e7e0151cc26c9df2808b2316ba57b453f0d57663
Szymon Głomski <szymon.glomski@gmail.com>
2024-10-13 13:55:33 +0200

    file_path -> json_file_path

--- dags/single_flow_ner_workflow.py
+++ dags/single_flow_ner_workflow.py
@@ -23,26 +23,21 @@ default_args = {
     'trigger_rule': TriggerRule.NONE_FAILED
 }
 
-
-FS_CONN_ID = "fs_text_data"  # id of connection defined in Airflow UI
-FILE_NAME = "ner/*.txt"
 ES_CONN_ARGS = {
     "hosts": os.environ["ELASTIC_HOST"],
     "ca_certs": "/usr/share/elasticsearch/config/certs/ca/ca.crt",
     "basic_auth": ("elastic", os.environ["ELASTIC_PASSWORD"]),
     "verify_certs": True,
 }
-#file_path can be renamed to json_file path to improve code readability
+
 def validate_params(**context):
-    if "params" not in context or "file_path" not in context["params"] or "fs_conn_id" not in context["params"]:
+    if "params" not in context or "json_file_path" not in context["params"] or "fs_conn_id" not in context["params"]:
         raise AirflowConfigException("No params defined")
 
-def choose_branch(**context):
-    json_args = JsonArgs(context["params"]["fs_conn_id"],context["params"]["file_path"])
+def decide_about_translation(**context):
+    json_args = JsonArgs(context["params"]["fs_conn_id"],context["params"]["json_file_path"])
     language = json_args.get_value("language")
-    if language != "en":
-        return "translate"
-    return "dummy"
+    return language != "en"
 
 with DAG(
         'single_flow_ner_workflow',
@@ -50,7 +45,7 @@ with DAG(
         schedule_interval=None,
         render_template_as_native_obj=True,  # REQUIRED TO RENDER TEMPLATE TO NATIVE LIST INSTEAD OF STRING!!!
         params={
-            "file_path": Param(
+            "json_file_path": Param(
                 type="string",
             ),
             "fs_conn_id": Param(
@@ -66,7 +61,7 @@ with DAG(
 
     detect_language_task = JsonLangdetectOperator(
         task_id="detect_language",
-        json_file_path="{{params.file_path}}",
+        json_file_path="{{params.json_file_path}}",
         fs_conn_id="{{params.fs_conn_id}}",
         input_key="content",
         output_key="language",
@@ -78,11 +73,9 @@ with DAG(
         ignore_downstream_trigger_rules = False
     )
 
-    dummy = DummyOperator(task_id="dummy")
-
     translate_task = JsonTranslateOperator(
         task_id="translate",
-        json_file_path="{{params.file_path}}",
+        json_file_path="{{params.json_file_path}}",
         fs_conn_id="{{params.fs_conn_id}}",
         input_key="content",
         output_key="translated",
@@ -93,7 +86,7 @@ with DAG(
         task_id="detect_entities",
         model="en_core_web_md",
         fs_conn_id="{{params.fs_conn_id}}",
-        json_file_path="{{params.file_path}}",
+        json_file_path="{{params.json_file_path}}",
         input_key="translated",
         output_key="ner"
     )
@@ -101,7 +94,7 @@ with DAG(
     es_push_task = ElasticJsonPushOperator(
         task_id="elastic_push",
         fs_conn_id="{{params.fs_conn_id}}",
-        json_file_path="{{params.file_path}}",
+        json_file_path="{{params.json_file_path}}",
         input_key="ner",
         index="ner",
         es_conn_args=ES_CONN_ARGS,
@@ -109,7 +102,7 @@ with DAG(
 
     stats_task = NerJsonStatisticsOperator(
         task_id="generate_stats",
-        json_file_path="{{params.file_path}}",
+        json_file_path="{{params.json_file_path}}",
         fs_conn_id="{{params.fs_conn_id}}",
         input_key="ner",
         output_key="ner_stats",
@@ -126,13 +119,13 @@ with DAG(
     summary_task = JsonSummaryMarkdownOperator(
         task_id="summary",
         summary_filename="summary.md",
-        output_dir='{{ "/".join(params["file_path"].split("/")[:-1] + ["summary"])}}',
+        output_dir='{{ "/".join(params["json_file_path"].split("/")[:-1] + ["summary"])}}',
         fs_conn_id="{{params.fs_conn_id}}",
 
         # this method works too, might be useful if we pull data with different xcom keys
         # stats="[{{task_instance.xcom_pull(task_ids = 'generate_stats', key = 'stats')}}, {{task_instance.xcom_pull(task_ids = 'translate', key = 'stats')}}]"
 
-        json_file_path="{{params.file_path}}",
+        json_file_path="{{params.json_file_path}}",
         input_key="ner_stats",
     )
 
