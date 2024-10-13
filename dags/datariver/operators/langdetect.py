926689f9b19eb8d11a7be7418d6a3fcccea74c2a
Szymon Głomski <szymon.glomski@gmail.com>
2024-10-13 10:35:22 +0200

    rename functions

--- dags/datariver/operators/langdetect.py
+++ dags/datariver/operators/langdetect.py
@@ -33,7 +33,7 @@ class LangdetectOperator(BaseOperator):
         return langs        # returning value from execute() is equivalent to 
                             # context["ti"].xcom_push(key="return_value", value=langs)
 
-class JsonLangdetectOperator(JsonArgsBaseOperator):
+class JsonLangdetectOperator(BaseOperator, JsonArgsBaseOperator):
     template_fields = ("json_path", "fs_conn_id", "input_key", "output_key", "encoding")
 
     def __init__(self, *, json_path, fs_conn_id="fs_default", input_key, output_key, encoding="utf-8", **kwargs):
@@ -49,6 +49,6 @@ class JsonLangdetectOperator(JsonArgsBaseOperator):
         hook = FSHook(self.fs_conn_id)
         basepath = hook.get_path()
         full_path = os.path.join(basepath, self.json_path)
-        text = self.get_value_from_json_file(full_path, self.encoding, self.input_key)
+        text = self.get_value(full_path, self.encoding, self.input_key)
         lang = langdetect.detect(text)
-        self.add_value_to_json_file(full_path, self.encoding, self.output_key, lang)
+        self.add_value(full_path, self.encoding, self.output_key, lang)
