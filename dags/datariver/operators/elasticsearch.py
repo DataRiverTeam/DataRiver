b9a80771b1e50a4c9754dbd8ca54c5e44706df77
Kacper Mazurczyk <kacper1609@wp.pl>
2024-10-22 12:24:07 +0200

    - added returned value from ElasticJsonPushOperator - reformatted code in elasticsearch module

--- dags/datariver/operators/elasticsearch.py
+++ dags/datariver/operators/elasticsearch.py
@@ -2,14 +2,13 @@ from airflow.models.baseoperator import BaseOperator
 
 from datariver.operators.json_tools import JsonArgs
 
-class ElasticPushOperator(BaseOperator):
-    template_fields = ("document", "fs_conn_id")
 
-    def __init__(self, *, index, document, fs_conn_id="fs_data", es_conn_args={}, **kwargs):
+class ElasticPushOperator(BaseOperator):
+    template_fields = ("index", "document")
+
+    def __init__(self, *, index, document, es_conn_args={}, **kwargs):
         super().__init__(**kwargs)
 
-        # note: fs_conn_id is probably useless in the elasticsearch operators
-        self.fs_conn_id = fs_conn_id
         self.index = index
         self.document = document
         self.es_conn_args = es_conn_args
@@ -28,6 +27,7 @@ class ElasticPushOperator(BaseOperator):
 
 
 class ElasticSearchOperator(BaseOperator):
+    template_fields = ("index", "query")
 
     def __init__(
         self,
@@ -39,7 +39,6 @@ class ElasticSearchOperator(BaseOperator):
         **kwargs
     ):
         super().__init__(**kwargs)
-        self.fs_conn_id = fs_conn_id
         self.index = index
         self.query = query
         self.es_conn_args = es_conn_args
@@ -57,33 +56,61 @@ class ElasticSearchOperator(BaseOperator):
 
         return result.body
 
+
 class ElasticJsonPushOperator(BaseOperator):
     template_fields = ("fs_conn_id", "json_file_path", "input_key", "encoding")
 
-    def __init__(self, *, index, fs_conn_id="fs_data", es_conn_args={}, json_file_path, input_key, encoding="utf-8", **kwargs):
+    def __init__(
+        self,
+        *,
+        index,
+        fs_conn_id="fs_data",
+        es_conn_args={},
+        json_file_path,
+        input_key,
+        encoding="utf-8",
+        refresh=False,
+        **kwargs
+    ):
         super().__init__(**kwargs)
 
-        # note: fs_conn_id is probably useless in the elasticsearch operators
         self.fs_conn_id = fs_conn_id
         self.index = index
         self.es_conn_args = es_conn_args
         self.json_file_path = json_file_path
         self.input_key = input_key
         self.encoding = encoding
+        self.refresh = refresh
 
-#pre_execute = lambda self: setattr(self["task"],"document",{"document": list(self["task_instance"].xcom_pull("detect_entities"))}),
+        # pre_execute = lambda self: setattr(self["task"],"document",{"document": list(self["task_instance"].xcom_pull("detect_entities"))}),
 
     def execute(self, context):
         from elasticsearch import Elasticsearch
-        json_args = JsonArgs(self.fs_conn_id, self.json_file_path, self.encoding)
+        json_args = JsonArgs(
+            self.fs_conn_id,
+            self.json_file_path,
+            self.encoding
+        )
+
         document = {}
+        # FIXME: remove "document" key from the index document,
+        # the dict should be indexed directly
+        # FIXME: the operator shouldn't be responsible for shaping the doc;
+        # a valid doc should be passed as argument,
+        # and the only responsibilities of the operator
+        # should be elasticsearch related
         document["document"] = list(json_args.get_value(self.input_key))
 
         es = Elasticsearch(
             **self.es_conn_args
         )
-        es.index(
+
+        response = es.index(
             index=self.index,
             document=document
         )
-        es.indices.refresh(index=self.index)
+
+        if self.refresh:
+            es.indices.refresh(index=self.index)
+
+        return response.body
